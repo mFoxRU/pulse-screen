65bbfaccf5aac82aa80129855b7cb6ad5cf68fbd
mFoxRU <mfoxru@gmail.com>
2014-11-20 14:09:19 +0300

    Added: Splitting plots

--- plotting.py
+++ plotting.py
@@ -13,18 +13,33 @@ def animate(i, lines, stream):
     return lines
 
 
-def plotter(stream):
-    fig = plot.figure()
-    ax = plot.axes(xlim=(0, stream.lim), ylim=(0, 256))
-    lines = []
-    for chan in xrange(stream.channels):
-        line, = ax.plot([], [], label='Channel {0}'.format(chan+1))
-        lines.append(line)
-    plot.xlabel('Time')
+def style_plot():
     plot.ylabel('Value')
     plot.grid()
     plot.legend(loc='upper right')
 
+
+def plotter(stream, split_streams=True):
+    lines = []
+    fig = plot.figure()
+
+    if not split_streams:
+        ax = plot.axes(xlim=(0, stream.lim), ylim=(0, 256))
+        for chan in xrange(stream.channels):
+            line, = ax.plot([], [], label='Channel {0}'.format(chan+1))
+            lines.append(line)
+        style_plot()
+    else:
+        for chan in xrange(stream.channels):
+            qax = fig.add_subplot(311+chan, xlim=(0, stream.lim), ylim=(0, 256))
+            # x = plot.subplot(210+chan)
+            line, = qax.plot([], [], label='Channel {0}'.format(chan+1))
+            style_plot()
+            lines.append(line)
+
+
+
+    plot.xlabel('Time')
     animus = anim.FuncAnimation(fig, animate,
                                 fargs=(lines, stream), interval=10, blit=False)
     plot.show()
